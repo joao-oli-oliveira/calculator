da6b607a1a4a4c500e9f350b6767458eda7b06f3
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-28 16:44:13 +0000

    Adds the basic HTML structure and a TODO file

---
+++ javascript.js
@@ -0,0 +1,34 @@
+// Operation functions
+const add = function (x, y) {
+  return x + y;
+};
+
+const subtract = function (x, y) {
+  return x - y;
+};
+
+const sum = function (arr) {
+  const sumOfAllNums = arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return sumOfAllNums;
+};
+
+const multiply = function (arr) {
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
+};
+
+const power = function (x, y) {
+  return x ** y;
+};
+
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
+  if (n > 0) {
+    return n * factorial(n - 1);
+  }
+};
