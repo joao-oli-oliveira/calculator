6b0b0bc7e63816d4a6a3a964d3bf74a29b772773
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-29 18:17:25 +0000

    Adds operation functions and event listeners

--- javascript.js
+++ javascript.js
@@ -1,34 +1,64 @@
-// Operation functions
-const add = function (x, y) {
-  return x + y;
-};
+let setOperator = null;
+let setResult = 0;
+let setPreviousResult = 0;
+let setFirstNumber = 0;
+let setSecondNumber = 0;
 
-const subtract = function (x, y) {
-  return x - y;
-};
+document.querySelectorAll("button").forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    console.log(btn.id);
+  })
+);
 
-const sum = function (arr) {
-  const sumOfAllNums = arr.reduce((total, currentItem) => {
-    return total + currentItem;
-  }, 0);
-  return sumOfAllNums;
-};
+/* 
 
-const multiply = function (arr) {
-  return arr.reduce((total, currentItem) => {
-    return total * currentItem;
-  }, 1);
-};
 
-const power = function (x, y) {
-  return x ** y;
-};
 
-const factorial = function (n) {
-  if (n === 0) {
-    return 1;
+
+*/
+
+function operation(firstNumber, secondNumber, OpFunction) {
+  if (setPreviousResult === 0) {
+    setResult = OpFunction(firstNumber, secondNumber);
+    setPreviousResult = setResult;
+    return setResult;
   }
-  if (n > 0) {
-    return n * factorial(n - 1);
-  }
+  return OpFunction(setPreviousResult, secondNumber);
+}
+
+const opFunctions = {
+  add: function (x, y) {
+    return x + y;
+  },
+
+  // subtract: function (x, y) {
+  //   return x - y;
+  // },
+  // sum: function (arr) {
+  //   const sumOfAllNums = arr.reduce((total, currentItem) => {
+  //     return total + currentItem;
+  //   }, 0);
+  //   return sumOfAllNums;
+  // },
+
+  // multiply: function (arr) {
+  //   return arr.reduce((total, currentItem) => {
+  //     return total * currentItem;
+  //   }, 1);
+  // },
+
+  // power: function (x, y) {
+  //   return x ** y;
+  // },
+
+  // factorial: function (n) {
+  //   if (n === 0) {
+  //     return 1;
+  //   }
+  //   if (n > 0) {
+  //     return n * this.factorial(n - 1);
+  //   }
+  // },
 };
+
+console.log(operation(setFirstNumber, setSecondNumber, opFunctions.add));
